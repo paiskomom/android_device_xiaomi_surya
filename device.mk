a5b5c6b7a6df8fee11aef22c4fc0b6c541e5940b
kssrao13882 <kssrao13882@gmail.com>
2022-03-31 07:03:28 +0000

    surya: Import pixel_2016_exclusive.xml
    This allows Unlimited Backup in Google Photos in any Quality
    
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- device.mk
+++ device.mk
@@ -180,6 +180,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
+# Google Photos
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti \
